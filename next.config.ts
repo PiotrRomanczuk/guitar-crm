10810a34937fd870cb35fbaab6865323cf9350c5
romanczukpiotr <p.romanczuk@gmail.com>
2025-11-10 01:22:23 +0100

    fix: add allowedDevOrigins to suppress cross-origin warning

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['piotrs-macbook-air.local'],
 };
 
 export default nextConfig;
