f428b52c76b430dae86bf2148c53d1dc4291264a
Piotr Romanczuk <p.romanczuk@gmail.com>
2025-12-09 17:18:09 +0100

    refactor(e2e): split admin songs workflow into granular tests and cleanup legacy tests

--- cypress/legacy/auth_dashboard_redirect.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('Dashboard auth redirects', () => {
-  it('redirects unauthenticated user from /dashboard to /sign-in', () => {
-    cy.clearCookies();
-    cy.visit('/dashboard', { failOnStatusCode: false });
-    cy.location('pathname').should('eq', '/sign-in');
-    // cy.location('search').should('contain', 'redirect=%2Fdashboard'); // This might vary
-  });
-});
