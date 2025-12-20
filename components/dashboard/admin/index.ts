58c177e32eeb10b45c3371837f6be3492d1e8864
romanczukpiotr <p.romanczuk@gmail.com>
2025-12-20 10:45:25 +0100

    chore: restructure components folder

--- components/dashboard/admin/index.ts
+++ components/dashboard/admin/index.ts
@@ -1,6 +1,2 @@
 export { AdminActionCard } from './AdminActionCard';
 export { AdminStatCard } from './AdminStatCard';
-export { UserCard } from './UserCard';
-export { UserForm } from './UserForm';
-export { UserList } from './UserList';
-export { UserTableRow } from './UserTableRow';
